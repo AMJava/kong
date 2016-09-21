9fb1f8f6375faee391fc5ac59d98aee03566f01a
Arturs <asirius4@inbox.lv>
2016-09-21 12:33:03 +0300

    Update sender.lua

--- kong/plugins/customhttp-log/sender.lua
+++ kong/plugins/customhttp-log/sender.lua
@@ -1,7 +1,9 @@
 local cjson = require "cjson"
 local url = require "socket.url"
 
-function _M.add_entry(_ngx, req_body_str, resp_body_str)
+local _M = {}
+
+function _M:add_entry(_ngx, req_body_str, resp_body_str)
   if not self.entries then
     return nil, "no entries table"
   elseif not _ngx then

f749592cfeacf7e1c8281f3d4bed98cae849224c
Arturs <asirius4@inbox.lv>
2016-09-21 12:26:12 +0300

    Update sender.lua

--- kong/plugins/customhttp-log/sender.lua
+++ kong/plugins/customhttp-log/sender.lua
@@ -1,1 +1,14 @@
+local cjson = require "cjson"
+local url = require "socket.url"
 
+function add_entry(_ngx, req_body_str, resp_body_str)
+  if not self.entries then
+    return nil, "no entries table"
+  elseif not _ngx then
+    return nil, "arg #1 (_ngx) must be given"
+  elseif req_body_str ~= nil and type(req_body_str) ~= "string" then
+    return nil, "arg #2 (req_body_str) must be a string"
+  elseif resp_body_str ~= nil and type(resp_body_str) ~= "string" then
+    return nil, "arg #3 (resp_body_str) must be a string"
+  end
+end

40aeb3dd1a3c3d66992f8d3adb051f646e641f3f
Arturs <asirius4@inbox.lv>
2016-09-21 15:29:17 +0300

    Update handler.lua

--- kong/plugins/customhttp-log/handler.lua
+++ kong/plugins/customhttp-log/handler.lua
@@ -25,6 +25,7 @@ entries = {}
 -- @param `message`  Message to be logged
 -- @return `body` http payload
 local function generate_post_payload(method, parsed_url, body)
+  ngx.log(ngx.ERR, "Error ":..tostring(body)..": ", "")
   return string.format(
     "%s %s HTTP/1.1\r\nHost: %s\r\nConnection: Keep-Alive\r\nContent-Type: application/json\r\nContent-Length: %s\r\n\r\n%s",
     method:upper(), parsed_url.path, parsed_url.host, string.len(body), body)

1d9a68b75f8a7c1f5712092f1ca03a74eb6a5d17
Arturs <asirius4@inbox.lv>
2016-09-22 11:34:14 +0300

    Update schema.lua

--- kong/plugins/customhttp-log/schema.lua
+++ kong/plugins/customhttp-log/schema.lua
@@ -3,7 +3,7 @@ return {
     http_endpoint = { required = true, type = "url" },
     method = { default = "POST", enum = { "POST", "PUT", "PATCH" } },
     log_bodies = {type = "boolean", default = false},
-    max_size = { default = 20, type = "number" },
+    max_size_mb = { default = 20, type = "number" },
     timeout = { default = 10000, type = "number" },
     keepalive = { default = 60000, type = "number" }
   }

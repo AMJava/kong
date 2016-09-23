d803e71cf8dcdab0d6fb262a0d5408e9da3bcbea
Arturs <asirius4@inbox.lv>
2016-09-23 13:29:56 +0300

    Update schema.lua

--- kong/plugins/bufferhttp-log/schema.lua
+++ kong/plugins/bufferhttp-log/schema.lua
@@ -5,9 +5,7 @@ return {
     flush_timeout = {type = "number", default = 2},
     log_bodies = {type = "boolean", default = false},
     connection_timeout = {type = "number", default = 30},
-    host = {type = "string", required = true, default = "collector.galileo.mashape.com"},
-    port = {type = "number", required = true, default = 443},
-    https = {type = "boolean", default = true},
+    endpoint = {type = "string", required = true, default = "http://"},
     https_verify = {type = "boolean", default = false}
   }
 }

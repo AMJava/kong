0b11de9a119f722d7f10bbb74942a87cf4ea290f
Arturs <asirius4@inbox.lv>
2016-09-23 16:40:42 +0300

    Update alf.lua

--- kong/plugins/bufferhttp-log/alf.lua
+++ kong/plugins/bufferhttp-log/alf.lua
@@ -155,7 +155,7 @@ function _M:add_entry(_ngx, req_body_str, resp_body_str)
 local max_size_mb = self.max_msg_size * 2^20
 
 if #cjson.encode(entries[idx]) > max_size_mb then
-ngx.log(WARN, "Message size is greater then max_size param: "..#cjson.encode(entries[idx])..">msx_max_size:"..self.max_msg_size.." MB")
+ngx.log(WARN, "Message size is greater then max_size param: "..#cjson.encode(entries[idx])..">msx_max_size:"..max_size_mb)
 entries[idx].payload.request.body = ""
 entries[idx].payload.response.body = ""
 end

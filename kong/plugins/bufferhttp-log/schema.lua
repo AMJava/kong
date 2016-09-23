5dd29174d8f0c214d510038b86cfc107fefe1f5b
Arturs <asirius4@inbox.lv>
2016-09-23 15:58:14 +0300

    Update schema.lua

--- kong/plugins/bufferhttp-log/schema.lua
+++ kong/plugins/bufferhttp-log/schema.lua
@@ -2,9 +2,9 @@ return {
   fields = {
     retry_count = {type = "number", default = 10},
     queue_size = {type = "number", default = 1000},
-    queue_sizeMB = {type = "number", default = 20},
-    max_msg_sizeMB = {type = "number", default = 2},
-    max_sending_queue_sizeMB = {type = "number", default = 200},
+    queue_size_mb = {type = "number", default = 20},
+    max_msg_size_mb = {type = "number", default = 2},
+    max_sending_queue_size_mb = {type = "number", default = 200},
     flush_timeout = {type = "number", default = 2},
     log_bodies = {type = "boolean", default = false},
     connection_timeout = {type = "number", default = 30},

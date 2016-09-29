56f353918f609e079b93e99ff96de66eacc1b200
Arturs <asirius4@inbox.lv>
2016-09-29 17:01:50 +0300

    Update alf.lua

--- kong/plugins/bufferhttp-log/alf.lua
+++ kong/plugins/bufferhttp-log/alf.lua
@@ -125,7 +125,7 @@ function _M:add_entry(_ngx, req_body_str, resp_body_str)
   self.entries[idx] = {
     source = "debessmana",
     timestamp = req_start_time()*1000,
-    timestamp1 = now()*1000,
+    timestamp1 = now*1000,
     id = uuid(),
     name = "KONG_API",
     headers = request_headers,

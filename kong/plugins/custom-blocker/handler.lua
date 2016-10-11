8c40a82708c639aa321155be4bf8dc89d0d12e7f
Arturs <asirius4@inbox.lv>
2016-10-11 17:14:45 +0300

    Create handler.lua

---
+++ kong/plugins/custom-blocker/handler.lua
@@ -0,0 +1,32 @@
+local BasePlugin = require "kong.plugins.base_plugin"
+
+--Extend Base Plugin
+local CustomBlocker = BasePlugin:extend()
+
+--Set Priority
+CustomBlocker.PRIORITY = 1001
+
+function CustomBlocker:new()
+  CustomBlocker.super.new(self, "custom-blocker")
+end
+
+function BCustomBlocker:access(conf)
+  CustomBlocker.super.access(self)
+  
+  if conf.block_entry then
+    responses.send_HTTP_FORBIDDEN("This service is not available right now")
+  end
+
+end
+
+function CustomBlocker:body_filter(conf)
+  CustomBlocker.super.body_filter(self)
+
+end
+
+function CustomBlocker:log(conf)
+  CustomBlocker.super.log(self)
+
+end
+
+return CustomBlocker

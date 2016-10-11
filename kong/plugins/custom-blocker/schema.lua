993f8fac06366f9ce1eed2091cf071107f70ded1
Arturs <asirius4@inbox.lv>
2016-10-11 17:39:59 +0300

    Update schema.lua

--- kong/plugins/custom-blocker/schema.lua
+++ kong/plugins/custom-blocker/schema.lua
@@ -1,5 +1,7 @@
 return {
   fields = {
-    block_entry = {type = "boolean", default = false},
+    error_code = {type = "number", default = 403},
+    error_message = {type = "string", required = true, default = "This service is not available right now"},
+    block_entry = {type = "boolean", default = false}
   }
 }

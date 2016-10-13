f7ba1887da3fc4c92de3d88f3e685f6063306c9c
Arturs <asirius4@inbox.lv>
2016-10-13 14:55:03 +0300

    Update schema.lua

--- kong/plugins/mocker/schema.lua
+++ kong/plugins/mocker/schema.lua
@@ -3,7 +3,7 @@ return {
     error_code = {type = "number", default = 403},
     error_message = {type = "string", required = true, default = "This service is not available right now"},
     content_type_json = {type = "boolean", default = true},
-    use_query_params = {type = "boolean", default = false},
+    use_url_params = {type = "boolean", default = false},
     mock_name_mapping = {type = "string", default = "{['mock1']={['query_param_mappings']={['param1']='1',['param2']='1'},['request_path_mappings']='/customer'}},['mock2']={['query_param_mappings']={['param1']='2',['param2']='2'},['request_path_mappings']='/product'}}"},
     mock_value_mapping = {type = "string", default = "{['mock1']={['code']=404,['contentType']='application/json; charset=utf-8',['message']='{\"message\":\"Service is Not Available\"}'},['mock2']={['code']=403,['contentType']='text/html; charset=UTF-8',['message']='<html><h1>Service is Not Available</h1></html>'}}"},
   }

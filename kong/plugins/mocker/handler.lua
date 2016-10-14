aaa137ec3955230fb87c0cd3fa4d4890fac9db6a
Arturs <asirius4@inbox.lv>
2016-10-14 12:28:12 +0300

    Update handler.lua

--- kong/plugins/mocker/handler.lua
+++ kong/plugins/mocker/handler.lua
@@ -98,9 +98,9 @@ function Mocker:access(conf)
     end
 		
     --find needed mock response 
-    if queryParams ~= nil or path then
+    if queryParams ~= nil then
          for keyMAP, valMAP in pairs(queryNameMAP) do
-		ngx.log(ngx.ERR, "TEST 2 "..valMAP,"")
+		ngx.log(ngx.ERR, "TEST 2 "..valMAP..":"..keyMAP,"")
 		if type(keyMAP) == "string" then
 			-- if query param
 			if string.sub(keyMAP, 0, 1) == "?" and queryParams ~= nil then

a53501da8725b4d2582ab915bd21102cd53792e4
Arturs <asirius4@inbox.lv>
2016-11-08 18:00:24 +0200

    Update schema.lua

--- kong/plugins/bufferhttp-log/schema.lua
+++ kong/plugins/bufferhttp-log/schema.lua
@@ -8,10 +8,11 @@ return {
     flush_timeout = {type = "number", default = 2},
     log_bodies = {type = "boolean", default = false},
     connection_timeout = {type = "number", default = 30},
-    endpoint = {type = "string", required = true, default = "test"},
+    endpoint = {type = "string", required = true, default = "http://"},
     https_verify = {type = "boolean", default = false},
     secure_message = {type = "boolean", default = false},
     add_request_id = {type = "boolean", default = true},
-    secure_patterns = {type = "array", default = { "(assword\":)\"(.-)\"", "(token\":)\"(.-)\""}},    
+    secure_patterns = {type = "array", default = { "(assword\":)\"(.-)\"", "(token\":)\"(.-)\""}},
+    default_app_key = {type = "string", default = "default app key"},
   }
 }

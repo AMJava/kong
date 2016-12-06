9b18d26d675c5e61587819cd4ff7c524c961fe14
Arturs <asirius4@inbox.lv>
2016-12-06 13:29:04 +0200

    Update handler.lua

--- kong/plugins/bufferhttp-log/handler.lua
+++ kong/plugins/bufferhttp-log/handler.lua
@@ -30,22 +30,22 @@ function BufferHTTPHandler:access(conf)
     end
   end
     
-  if conf.log_bodies then
+ -- if conf.log_bodies then
     read_body()
     ngx.ctx.bufferhttp = {req_body = get_body_data()}
-  end
+ -- end
 end
 
 function BufferHTTPHandler:body_filter(conf)
   BufferHTTPHandler.super.body_filter(self)
   ngx.log(ngx.ERR, "In body filter ", "")
-  if conf.log_bodies then
+  --if conf.log_bodies then
     local chunk = ngx.arg[1]
     local ctx = ngx.ctx
     local res_body = ctx.bufferhttp and ctx.bufferhttp.res_body or ""
     res_body = res_body .. (chunk or "")
     ctx.bufferhttp.res_body = res_body
-  end
+ -- end
 end
 
 function BufferHTTPHandler:header_filter()

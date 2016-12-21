b61fc4db6cca76a907ad24bd977dac5f42fd9076
Arturs <asirius4@inbox.lv>
2016-12-21 14:06:04 +0200

    Update handler.lua

--- kong/plugins/oauth2/handler.lua
+++ kong/plugins/oauth2/handler.lua
@@ -9,10 +9,12 @@ end
 
 function OAuthHandler:access(conf)
   OAuthHandler.super.access(self)
+    local path_prefix = (ngx.ctx.api.request_path and pl_stringx.startswith(ngx.var.request_uri, ngx.ctx.api.request_path)) and ngx.ctx.api.request_path or ""
   ngx.log(ngx.ERR, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: "..ngx.ctx.api.request_path, "")
   ngx.log(ngx.ERR, "BBBBBBBBBBBBBBBBBBBBBBBBBBBBB: "..ngx.var.request_uri, "")
   ngx.log(ngx.ERR, "CCCCCCCCCCCCCCCCCCCCCCCCCCCC: "..ngx.ctx.api.request_path, "")
   ngx.log(ngx.ERR, "DDDDDDDDDDDDDDDDDDDDDDDDDDD: "..ngx.ctx.api.request_path, "")
+  ngx.log(ngx.ERR, "EEEEEEEEEEEEEEEEEEEEEEEEEEE: "..path_prefix, "")
   access.execute(conf)
 end
 

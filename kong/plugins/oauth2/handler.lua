5f3dcd83dbe3332c1c84e229d4569b0b17e2a2f3
Arturs <asirius4@inbox.lv>
2016-12-21 14:11:07 +0200

    Update handler.lua

--- kong/plugins/oauth2/handler.lua
+++ kong/plugins/oauth2/handler.lua
@@ -1,6 +1,5 @@
 local BasePlugin = require "kong.plugins.base_plugin"
 local access = require "kong.plugins.oauth2.access"
-local pl_stringx = require "pl.stringx"
 
 local OAuthHandler = BasePlugin:extend()
 
@@ -10,12 +9,10 @@ end
 
 function OAuthHandler:access(conf)
   OAuthHandler.super.access(self)
-    local path_prefix = (ngx.ctx.api.request_path and pl_stringx.startswith(ngx.var.request_uri, ngx.ctx.api.request_path)) and ngx.ctx.api.request_path or ""
-  ngx.log(ngx.ERR, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: "..ngx.ctx.api.request_path, "")
+  if ngx.req.get_method() == "POST" then
+    
+  end
   ngx.log(ngx.ERR, "BBBBBBBBBBBBBBBBBBBBBBBBBBBBB: "..ngx.var.request_uri, "")
-  ngx.log(ngx.ERR, "CCCCCCCCCCCCCCCCCCCCCCCCCCCC: "..ngx.ctx.api.request_path, "")
-  ngx.log(ngx.ERR, "DDDDDDDDDDDDDDDDDDDDDDDDDDD: "..ngx.ctx.api.request_path, "")
-  ngx.log(ngx.ERR, "EEEEEEEEEEEEEEEEEEEEEEEEEEE: "..path_prefix, "")
   access.execute(conf)
 end
 

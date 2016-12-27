8d765d65642cec58495e008e823a6f43e2337338
Arturs <asirius4@inbox.lv>
2016-12-27 13:36:22 +0200

    Update alf.lua

--- kong/plugins/bufferhttp-log/alf.lua
+++ kong/plugins/bufferhttp-log/alf.lua
@@ -145,11 +145,11 @@ function _M:add_entry(_ngx, req_body_str, resp_body_str,conf)
       response_content = resp_body_str
 
       if request_auth2_credetionals then
+	ngx.log(ngx.ERR, "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB:"..request_auth2_credetionals, "")			
         request_headers["dm_identity"]= ngx.decode_base64(request_auth2_credetionals)
       end
 			
       request_headers["dm_auth2_token"]= "TEST"
-      ngx.log(ngx.ERR, "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB:"..request_auth2_credetionals, "")
     end		
   end
 
